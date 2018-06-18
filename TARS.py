153b35f8a222f755b83366972e110a4285ce2dae
Adarsh Punj <30762976+adarshpunj@users.noreply.github.com>
2018-06-18 05:49:29 +0530

    v2
    Added threading, and some other GUI changes.

--- TARS.py
+++ TARS.py
@@ -1,23 +1,28 @@
 import Tkinter as tk
-import tkMessageBox
+import tkFileDialog, ttk
 from Tkinter import *
 from bs4 import BeautifulSoup
 import requests
+import os
+import threading
 import time
-from selenium import webdriver
 switch=0
-def About():
-	tkMessageBox.showinfo("About","TARS Subtitle Downloader v1.0. Made with Love using Python.")
+def About(evnt=None):
+	os.system('''python -m webbrowser -t "https://github.com/adarshpunj/TARS"''')
 def Download():
 	user_querry = str(entry.get())
+	filename = user_querry
 	if user_querry=="":
 		tkMessageBox.showinfo("Error","Please enter a movie title.")
 		return
 	user_querry=user_querry.replace(' ','+')
 	default_link="http://www.imdb.com/find?&q="
 	imdb_link=default_link+user_querry
+	status.config(text="Querry Recieved")
 	try:
+		status.config(text="Sending Request...")
 		response=requests.get(imdb_link)
+		status.config(text="Request Sent")
 	except:
 		tkMessageBox.showinfo("Request Failed","Please check your internet connection, and try again.")
 		return
@@ -29,6 +34,7 @@ def Download():
 		tkMessageBox.showinfo("Error","The entered title is invalid. Please enter a valid title, and try again.")
 		return
 	imdb_code=link[:16][::-1][:-7][::-1]
+	status.config(text="Movie Located")
 	response=requests.get("http://www.yifysubtitles.com/movie-imdb/"+imdb_code)
 	html=response.text
 	soup=BeautifulSoup(html,'html.parser')
@@ -46,40 +52,47 @@ def Download():
 	for i in sorted(subtitles):
 	    linklist.append(subtitles[i])
 	if linklist==[]:
-	    tkMessageBox.showinfo("Failed!","Sorry! No subtitles found.")
+	    tkMessageBox.showinfo("Failed!","Sorry! No subtitles found. Please check for spelling errors")
 	else:
-	    url=linklist[-1].split('/')[2]
-	    driver=webdriver.Chrome("/Users/adarshpunj/MSD/chromedriver")
-	    driver.get(constant_url+url+'.zip')
-	    time.sleep(3)
-	    switch=1
-	    driver.quit()
+		url=linklist[-1].split('/')[2]
+		status.config(text="Subtitle Found")
+		root.directory = tkFileDialog.askdirectory()
+		file = requests.get(constant_url+url+'.zip',allow_redirects=True)
+		open(root.directory+'/'+filename+'-TARS'+'.zip','wb').write(file.content)
+		switch = 1
 	if switch==1:
-		tkMessageBox.showinfo("Success!","The subtitle has been successfully downloaded to the default Downloads directory!")
+		tkMessageBox.showinfo("Success!","The subtitle has been successfully downloaded!")
+def downloadThread(event):
+	global thread
+	thread = threading.Thread(target=Download)
+	thread.daemon=True
+	thread.start()
 if __name__ == '__main__':
 	root = tk.Tk()
 	root.title("TARS Subtitle Downloader")
 	root.geometry("500x400")
+	root.resizable(False, False)
 	label = tk.Label(root,text="   ")
 	label.pack()
 	label = tk.Label(root,text="   ")
 	label.pack()
 	label = tk.Label(root,text="   ")
 	label.pack()
+	logo = tk.PhotoImage(file="logo.gif")
 	label = tk.Label(root,text="ENTER MOVIE TITLE",font=("Helvetica",16))
 	label.pack()
 	entry = tk.Entry(root)
 	entry.pack()
-	button = tk.Button(root, text="Download Subtitle",command=Download)
+	button = tk.Button(root,text="Download Subtitle",font=("Sans",12),command=lambda:downloadThread(None))
 	button.pack()
+	status = tk.Label(root,text="",fg="lightblue",font=("Sans",18,"bold"))
+	status.pack(pady=60)
+	label = tk.Label(root,text="TARS Subtitle Downloader",font=("Sans",12,"bold"),fg='medium slate blue')
+	label.pack()
+	label = tk.Label(root,text="is a quick and free way to download movie subtitles",font=("Sans",12,"bold"),fg='light slate blue')
+	label.pack()
 	label = tk.Label(root,text="   ")
-	label.pack(padx=120,pady=50)
-	label = tk.Label(root,text="TARS Subtitle Downloader")
-	label.pack()
-	label = tk.Label(root,text="is a quick and free way to download movie subtitles")
-	label.pack()
-	label = tk.Label(root,text="   ")
-	label.pack()
-	button = tk.Button(root, text="i", font=("Courier",15,"bold"),command=About)
-	button.pack()
+	github = tk.Label(root, image=logo)
+	github.pack(pady=10)
+	github.bind('<Button-1>',About)
 	root.mainloop()
